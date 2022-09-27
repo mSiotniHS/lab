4913170f6860dfd05c4c8330d228d48574f7d00a
Artyom Abramyan <artemabramyan7@icloud.com>
2022-09-27 13:55:38 +0300

    Реализовал абстрактный класс Vehicle

---
+++ Vehicle.cs
@@ -0,0 +1,17 @@
+﻿namespace lab;
+
+public abstract class Vehicle
+{
+	public string Brand { get; set; }
+	public uint ReleaseYear { get; set; }
+	public double InitialPrice { get; set; }
+
+	public Vehicle(string brand, uint releaseYear, double initialPrice)
+	{
+		Brand = brand;
+		ReleaseYear = releaseYear;
+		InitialPrice = initialPrice;
+	}
+
+	public abstract double CalculatePrice();
+}
