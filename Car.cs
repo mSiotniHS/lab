c4a07af9c3a3ee8894c6c59e4538010cc7e034e2
Artyom Abramyan <artemabramyan7@icloud.com>
2022-09-27 13:55:52 +0300

    Реализовал наследников Car и Truck

---
+++ Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace lab;
+
+public class Car : Vehicle
+{
+	public Car(string brand, uint releaseYear, double initialPrice) : base(brand, releaseYear, initialPrice)
+	{
+	}
+
+	public override double CalculatePrice()
+	{
+		var currentYear = DateTime.Now.Year;
+		var difference = currentYear - ReleaseYear;
+		const double regressionFactor = 0.1;
+
+		return InitialPrice * Math.Pow(1 - regressionFactor, difference);
+	}
+}
---
+++ Truck.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace lab;
+
+public class Truck : Vehicle
+{
+	public Truck(string brand, uint releaseYear, double initialPrice) : base(brand, releaseYear, initialPrice)
+	{
+	}
+
+	public override double CalculatePrice()
+	{
+		var currentYear = DateTime.Now.Year;
+		var difference = currentYear - ReleaseYear;
+		var regressionFactor = difference switch
+		{
+			0 => 0,
+			<= 3 => 0.1,
+			<= 10 => 0.2,
+			_ => 0.3
+		};
+
+		return InitialPrice * (1 - regressionFactor);
+	}
+}
