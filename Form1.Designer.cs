0df91d3dddf2bb83f3e1f10afdf4eceacda44000
Artyom Abramyan <artemabramyan7@icloud.com>
2022-10-13 23:08:33 +0300

    Сделал общий вид формы

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,11 +29,97 @@ partial class Form1
 	/// </summary>
 	private void InitializeComponent()
 	{
-		this.components = new System.ComponentModel.Container();
-		this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-		this.ClientSize = new System.Drawing.Size(800, 450);
-		this.Text = "Form1";
+            this.textFieldsGroupBox = new System.Windows.Forms.GroupBox();
+            this.buttonsGroupBox = new System.Windows.Forms.GroupBox();
+            this.vehicleGridView = new System.Windows.Forms.DataGridView();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.vehicleGridView)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textFieldsGroupBox
+            // 
+            this.textFieldsGroupBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textFieldsGroupBox.Location = new System.Drawing.Point(3, 3);
+            this.textFieldsGroupBox.Name = "textFieldsGroupBox";
+            this.textFieldsGroupBox.Size = new System.Drawing.Size(238, 216);
+            this.textFieldsGroupBox.TabIndex = 0;
+            this.textFieldsGroupBox.TabStop = false;
+            this.textFieldsGroupBox.Text = "Ввод";
+            // 
+            // buttonsGroupBox
+            // 
+            this.buttonsGroupBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonsGroupBox.Location = new System.Drawing.Point(3, 225);
+            this.buttonsGroupBox.Name = "buttonsGroupBox";
+            this.buttonsGroupBox.Size = new System.Drawing.Size(238, 216);
+            this.buttonsGroupBox.TabIndex = 1;
+            this.buttonsGroupBox.TabStop = false;
+            this.buttonsGroupBox.Text = "Управление";
+            // 
+            // vehicleGridView
+            // 
+            this.vehicleGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.vehicleGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.vehicleGridView.Location = new System.Drawing.Point(3, 3);
+            this.vehicleGridView.Name = "vehicleGridView";
+            this.vehicleGridView.RowTemplate.Height = 25;
+            this.vehicleGridView.Size = new System.Drawing.Size(544, 444);
+            this.vehicleGridView.TabIndex = 2;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 250F));
+            this.tableLayoutPanel1.Controls.Add(this.vehicleGridView, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 3;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel2.Controls.Add(this.textFieldsGroupBox, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.buttonsGroupBox, 0, 1);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(553, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(244, 444);
+            this.tableLayoutPanel2.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.vehicleGridView)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
 	}
 
 	#endregion
+
+	private System.Windows.Forms.GroupBox textFieldsGroupBox;
+	private System.Windows.Forms.GroupBox buttonsGroupBox;
+	private System.Windows.Forms.DataGridView vehicleGridView;
+	private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+	private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
 }
