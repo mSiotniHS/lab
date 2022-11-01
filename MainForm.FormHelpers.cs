7b3990632b74385ae929dbedd675fc383fbff9a8
Artyom Abramyan <artemabramyan7@icloud.com>
2022-11-01 17:44:00 +0300

    [refactor] Вынес вспомогательные методы формы в отдельный файл

---
+++ MainForm.FormHelpers.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Globalization;
+
+namespace lab;
+
+partial class MainForm
+{
+	private (string, uint, double, VehicleType) CurrentInputState
+	{
+		get
+		{
+			var brand = VehicleBrandTextBox.Text;
+			var year = uint.Parse(VehicleReleaseYearTextBox.Text);
+			var price = double.Parse(VehicleInitialPriceTextBox.Text.Replace(',', '.'), CultureInfo.InvariantCulture);
+			var type = VehicleTypeComboBox.SelectedIndex switch
+			{
+				0 => VehicleType.Car,
+				1 => VehicleType.Truck,
+				_ => VehicleType.None
+			};
+
+			return (brand, year, price, type);
+		}
+		set
+		{
+			var (brand, year, price, type) = value;
+
+			VehicleBrandTextBox.Text = brand;
+			VehicleReleaseYearTextBox.Text = year.ToString();
+			VehicleInitialPriceTextBox.Text = price.ToString(CultureInfo.InvariantCulture);
+			VehicleTypeComboBox.SelectedIndex = type switch
+			{
+				VehicleType.Car => 0,
+				VehicleType.Truck => 1,
+				VehicleType.None => -1,
+				_ => throw new Exception("That's... unreal")
+			};
+		}
+	}
+
+	private bool IsFormFilled()
+	{
+		var brandIsFilled = VehicleBrandTextBox.Text != string.Empty;
+		var yearIsFilled = VehicleReleaseYearTextBox.Text != string.Empty;
+		var priceIsFilled = VehicleInitialPriceTextBox.Text != string.Empty;
+		var typeIsSelected = VehicleTypeComboBox.SelectedIndex != -1;
+
+		return brandIsFilled && yearIsFilled && priceIsFilled && typeIsSelected;
+	}
+
+	private void ClearForm()
+	{
+		VehicleBrandTextBox.Clear();
+		VehicleReleaseYearTextBox.Clear();
+		VehicleInitialPriceTextBox.Clear();
+		VehicleTypeComboBox.SelectedIndex = -1;
+	}
+}
--- MainForm.cs
+++ MainForm.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Windows.Forms;
 
 namespace lab;
@@ -33,60 +32,9 @@ public sealed partial class MainForm : Form
 		VehicleTypeComboBox.SelectedIndexChanged += OnFormChanged;
 	}
 
-	private bool IsFormFilled()
-	{
-		var brandIsFilled = VehicleBrandTextBox.Text != string.Empty;
-		var yearIsFilled = VehicleReleaseYearTextBox.Text != string.Empty;
-		var priceIsFilled = VehicleInitialPriceTextBox.Text != string.Empty;
-		var typeIsSelected = VehicleTypeComboBox.SelectedIndex != -1;
-
-		return brandIsFilled && yearIsFilled && priceIsFilled && typeIsSelected;
-	}
-
-	private (string, uint, double, VehicleType) CurrentInputState
-	{
-		get
-		{
-			var brand = VehicleBrandTextBox.Text;
-			var year = uint.Parse(VehicleReleaseYearTextBox.Text);
-			var price = double.Parse(VehicleInitialPriceTextBox.Text.Replace(',', '.'), CultureInfo.InvariantCulture);
-			var type = VehicleTypeComboBox.SelectedIndex switch
-			{
-				0 => VehicleType.Car,
-				1 => VehicleType.Truck,
-				_ => VehicleType.None
-			};
-
-			return (brand, year, price, type);
-		}
-		set
-		{
-			var (brand, year, price, type) = value;
-
-			VehicleBrandTextBox.Text = brand;
-			VehicleReleaseYearTextBox.Text = year.ToString();
-			VehicleInitialPriceTextBox.Text = price.ToString(CultureInfo.InvariantCulture);
-			VehicleTypeComboBox.SelectedIndex = type switch
-			{
-				VehicleType.Car => 0,
-				VehicleType.Truck => 1,
-				VehicleType.None => -1,
-				_ => throw new Exception("That's... unreal")
-			};
-		}
-	}
-
 	private void OnFormChanged(object? sender, EventArgs e)
 	{
 		SaveButton.Enabled = IsFormFilled();
-	}
-
-	private void ClearForm()
-	{
-		VehicleBrandTextBox.Clear();
-		VehicleReleaseYearTextBox.Clear();
-		VehicleInitialPriceTextBox.Clear();
-		VehicleTypeComboBox.SelectedIndex = -1;
 	}
 
 	private void VehicleGridView_CellClick(object sender, DataGridViewCellEventArgs e)
